e1fa642195213f6b0f35bf3a50cf75f7a2a166ff
AdelBoussa <aboussenane@gmail.com>
2024-02-10 20:39:50 -0800

    Up scale to 3

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <ArComponent markerPath="/Patterns/pattern-A.patt" modelPath="/Puppy/scene.gltf" scale="0.5 0.5 0.5" position="0 0 0" rotation="180 270 90" />
+      <ArComponent markerPath="/Patterns/pattern-A.patt" modelPath="/Puppy/scene.gltf" scale="3 3 3" position="0 0 0" rotation="180 270 90" />
       
     </>
   )

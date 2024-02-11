535adf7811dd355f9a332ecb1e335fef7fb50a7f
AdelBoussa <aboussenane@gmail.com>
2024-02-10 20:37:35 -0800

    reverted to hiro marker for testing

--- src/Components/ArComponent.jsx
+++ src/Components/ArComponent.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { Entity, Scene } from 'aframe-react';
 
 function ArComponent({markerPath, modelPath, scale, position, rotation}) {
+  /*
+  primitive="a-marker"
+        preset="custom" 
+        type="pattern"
+        url={markerPath}
+  */
   return (
     <Scene arjs='sourceType: webcam;smoothing: true; smoothingAmount: 0.9;'>
       <Entity
         primitive="a-marker"
-        preset="custom" 
-        type="pattern"
-        url={markerPath}
+        preset="hiro" 
+        
         
         >
         <Entity
